4b607240f4c1974f3ae58d772820638bf53f0189
Agailh <agailham86@gmail.com>
2023-05-25 16:45:41 +0700

    commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]' >
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white'>
-        <p>Hi, My name is  </p>
+        <p className='text-xl'>Hi, My name is  </p>
         <h1>Aga Ilham Ferdiansyah </h1>
         <h2>I'm a Front-End Developer. </h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi rerum, dicta deserunt nisi hic corporis ea exercitationem quo explicabo distinctio omnis, voluptate dolor sapiente voluptates placeat tempore, similique officiis laboriosam!</p>
