785cfe315b1d9f779ff28dba1d445d30c4f02f76
Agailh <agailham86@gmail.com>
2023-05-31 14:20:07 +0700

    home

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,11 +6,19 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]' >
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-white'>
-        <p className='text-xl'>Hi, My name is  </p>
-        <h1>Aga Ilham Ferdiansyah </h1>
-        <h2>I'm a Front-End Developer. </h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi rerum, dicta deserunt nisi hic corporis ea exercitationem quo explicabo distinctio omnis, voluptate dolor sapiente voluptates placeat tempore, similique officiis laboriosam!</p>
+        <p className='text-xl text-[#1d4b98]'>Hi, My name is  </p>
+        <h1 className='text-4xl sm:text-6xl font-bold '>Aga Ilham Ferdiansyah </h1>
+        <h2 className='text-xl sm:text-5xl font-bold text-[#bfbfbf]'>I'm a Front-End Developer. </h2>
+        <p className='text-[#bfbfbf] py-4 max-w'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi rerum, dicta deserunt nisi hic corporis ea exercitationem quo explicabo distinctio omnis, voluptate dolor sapiente voluptates placeat tempore, similique officiis laboriosam!</p>
 
+      <div>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#1d4b98] hover:border-[#1d4b98]'>
+         View Work
+          <span className=' group-hover:rotate-90 duration-300'>
+          <HiArrowNarrowRight  className='ml-3'/>
+          </span>
+        </button>
+      </div>
       </div>
 
     </div>
