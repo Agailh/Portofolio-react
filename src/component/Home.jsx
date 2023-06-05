77b95f234d491cae42fddc4635316ae40a9febcf
Agailh <agailham86@gmail.com>
2023-06-05 14:06:36 +0700

    commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <p className='text-xl text-[#1d4b98]'>Hi, My name is  </p>
         <h1 className='text-4xl sm:text-6xl font-bold '>Aga Ilham Ferdiansyah </h1>
         <h2 className='text-xl sm:text-5xl font-bold text-[#bfbfbf]'>I'm a Front-End Developer. </h2>
-        <p className='text-[#bfbfbf] py-4 max-w'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi rerum, dicta deserunt nisi hic corporis ea exercitationem quo explicabo distinctio omnis, voluptate dolor sapiente voluptates placeat tempore, similique officiis laboriosam!</p>
+        <p className='text-[#bfbfbf] py-4 max-w'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi rerum, dicta deserunt nisi hic corporis ea exercitationem quo explicabo distinctio omnis, voluptate dolor sapiente voluptates placeat tempore, similique officiis laboriosam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam, amet atque error velit eius ab dignissimos sunt dolorem eos, tempore soluta nam deleniti in quae voluptate vel natus, rem provident?</p>
 
       <div>
         <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#1d4b98] hover:border-[#1d4b98]'>
