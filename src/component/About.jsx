c2749effe3d9a24c08fb5c11770491ab37250e59
Agailh <agailham86@gmail.com>
2023-06-08 13:19:06 +0700

    about init

---
+++ src/component/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full h-screen bg-[#0a192f] text-white'>
+        <div className='flex flex-col justify-center items-center w-full h-full'>
+
+        </div>
+    </div>
+  )
+}
+
+export default About
