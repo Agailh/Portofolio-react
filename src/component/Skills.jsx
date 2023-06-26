d1fb591a199ee3dbdcb009cbd2bc6a670120e28a
Agailh <agailham86@gmail.com>
2023-06-26 14:42:00 +0700

    import skills

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+
+import HTML from "../Assets/html5.png"
+import Phaser from "../Assets/phaser.png"
+import CSS from "../Assets/css-icon.png"
+import Javascript from "../Assets/javascript.png"
+import Tailwind from "../Assets/tailwind-css-icon.png"
+import TFL from "../Assets/TFL.png"
+
+
+
 const Skills = () => {
   return (
     <div>Skills</div>
