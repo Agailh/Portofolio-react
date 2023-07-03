2765d37159c9e08e6261aabee005b2c0e3cef090
Agailh <agailham86@gmail.com>
2023-07-03 12:07:14 +0700

    skills grid

--- src/component/Skills.jsx
+++ src/component/Skills.jsx
@@ -12,18 +12,34 @@ import TFL from "../Assets/TFL.png"
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='bg-[#0a192f]'>
       {/* container */}
-      <div>
-        <div>
-          <p>Experience</p>
-          <p>// These are the technologies I've working with </p>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div >
+          <p className=' text-white text-4xl font-bold inline border-b-4 border-blue-600'>Experience</p>
+          <p className='text-[#bfbfbf] py-4'>// These are the technologies I've working with </p>
         </div>
 
-        <div>
-          <div>
-            <img src={HTML} alt='Html Icon' />
-            <p>HTML</p>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 '>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Phaser} alt='Html Icon' />
+            <p className='my-4 text-white'>Phaser</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt='Html Icon' />
+            <p className='my-4 text-white'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Javascript} alt='Html Icon' />
+            <p className='my-4 text-white'>Javascript</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt='Html Icon' />
+            <p className='my-4 text-white'>Tailwind</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={TFL} alt='Html Icon' />
+            <p className='my-4 text-white'>TFL</p>
           </div>
         </div>
       </div>
